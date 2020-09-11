fb89580df55dc6107e91bfc602849159ad19946e
Oscar Branson <oscarbranson@gmail.com>
2020-09-11 17:39:47 +0100

    working carbon calculation functions

--- carbon-live-js/src/components/csys.jsx
+++ carbon-live-js/src/components/csys.jsx
@@ -1,6 +1,8 @@
 // functions for calculating C chemistry
 
-export function calc_Ks({Tc, Sal}) {
+var BT35 = 415.7e-6 / 35.;
+
+export function calc_Ks({ Tc, Sal }) {
     let sqrtSal = Math.sqrt(Sal);
     let T = Tc + 273.15;
     let lnT = Math.log(T);
@@ -21,119 +23,101 @@ export function calc_Ks({Tc, Sal}) {
     }
 }
 
-// TA = 2 CO3 + HCO3 + BOH3 + OH - H
-// DIC = CO3 + HCO3 + CO2
+export function give_DIC_TA({ DIC, TA, Sal, Ks}) {
+    // Returns [H], units: mol/kg
 
-export function calc_csys({DIC, TA, Sal, Temp}) {
-    TA = TA * 1e-6;
-    DIC = DIC * 1e-6;
-
-    let BT = 416e-6 * Sal / 35;
-
-    let Ks = calc_Ks({Tc: Temp, Sal: Sal})
-
-    let pH = 8.;  // start pH
+    let BT = BT35 * Sal;
+    let pH = 8.;
     let H = 10 ** -pH;
     let diff_H = H;
     let tiny_diff_H = 1e-15;
 
-    let iter = 0;
+    let HCO3 = null;
+    let CO3 = null;
+    let CAlk = null;
+    let BAlk = null;
+    let OH = null;
+    let residual = null;
+    let slope = null;
 
-    let CA = null;
-    let a = null;
-    let b = null;
-    let c = null;
-    let H_old = null;
+    while (Math.abs(diff_H) > tiny_diff_H) {
+        HCO3 = DIC / (1 + H / Ks.K1 + Ks.K2 / H);
+        CO3 = DIC / (1 + H / Ks.K2 + H**2 / (Ks.K1 * Ks.K2))
+        CAlk = HCO3 + 2 * CO3;
+        BAlk = BT * Ks.KB / (Ks.KB + H);
+        OH = Ks.KW / H;
 
-    while (diff_H > tiny_diff_H) {
-        H_old = H;
+        residual = TA - CAlk - BAlk - OH + H;
+        slope = 1 + (2 * CO3 + HCO3 + OH) / H + BT * Ks.KB / (Ks.KB + H) ** 2
 
-        CA = TA - BT * (Ks.KB / (Ks.KB + H))
-
-        a = CA;
-        b = Ks.K1 * (CA - DIC);
-        c = Ks.K1 * Ks.K2 * (CA - 2 * DIC);
-        H = (-b + Math.sqrt(b**2 - 4 * a * c)) / (2 * a);
-
-        diff_H = Math.abs(H - H_old);
-        iter += 1;
+        diff_H = residual / slope;
+        H -= diff_H;
     }
 
-    pH = -Math.log10(H)
-    let CO2aq = CA / (Ks.K1 / H + 2 * Ks.K1 * Ks.K2 / H**2);
-    let HCO3 = 1e6 * DIC / (1 + H / Ks.K1 + Ks.K2 / H);
-    let CO3 = 1e6 * DIC / (1 + H / Ks.K2 + H**2 / (Ks.K1 * Ks.K2));
-    let pCO2 = CO2aq / Ks.K0 * 1e6;
+    return H
+}
 
+export function give_H_CO2({H, CO2, Ks}) {
+    // Returns DIC, units: mol/kg
+    return CO2 * (1 + Ks.K1 / H + Ks.K1 * Ks.K2 / H ** 2)  // DIC
+}
+
+// For adding CO2 with conserved TA.
+export function give_fCO2_TA({ fCO2, TA, Sal, Ks}) {
+    // Returns [H], units: mol/kg
+    let BT = BT35 * Sal;
+
+    let CO2 = fCO2 * Ks.K0;
+    let pH = 8.;
+    let H = 10 ** -pH;
+    let diff_H = H;
+    let tiny_diff_H = 1e-15;
+
+    let HCO3 = null;
+    let CO3 = null;
+    let CAlk = null;
+    let BAlk = null;
+    let OH = null;
+    let residual = null;
+    let slope = null;
+
+    while (Math.abs(diff_H) > tiny_diff_H) {
+        HCO3 = Ks.K1 * CO2 / H;
+        CO3 = Ks.K1 * Ks.K2 * CO2 / H ** 2;
+        CAlk = HCO3 + 2 * CO3;
+        BAlk = BT * Ks.KB / (Ks.KB + H);
+        OH = Ks.KW / H;
+
+        residual = TA - CAlk - BAlk - OH + H;
+        slope = 1 + (2 * CO3 + HCO3 + OH) / H + BT * Ks.KB / (Ks.KB + H) ** 2
+
+        diff_H = residual / slope;
+        H -= diff_H;
+    }
+
+    return H
+}
+
+export function calc_csys({ DIC, TA, Sal, Temp }) {
+    TA = TA * 1e-6;
+    DIC = DIC * 1e-6;
+
+    let Ks = calc_Ks({ Tc: Temp, Sal: Sal })
+
+    let H = give_DIC_TA({DIC: DIC, TA:TA, Sal: Sal, Ks:Ks})
+
+    let pH = -Math.log10(H)
+    let CO2 = 1e6 * DIC / (1 + Ks.K1 / H + Ks.K1 * Ks.K2 / H**2)
+    let HCO3 = 1e6 * DIC / (1 + H / Ks.K1 + Ks.K2 / H);
+    let CO3 = 1e6 * DIC / (1 + H / Ks.K2 + H ** 2 / (Ks.K1 * Ks.K2));
+    let fCO2 = CO2 / Ks.K0;
 
     return {
         pH: pH,
-        pCO2: pCO2,
-        CO2: CO2aq * 1e6,
+        fCO2: fCO2,
+        CO2: CO2 * 1e6,
         HCO3: HCO3,
         CO3: CO3,
     }
 }
 
-// function calc_DIC({pCO2, TA, Temp, Sal}) {
-//     let TempK = Temp + 273.15;
-//     TA = TA * 1e-6;
-//     let CO2 = pCO2 * 1e-6 * Ks.K0;
-
-//     let BT = 416e-6 * Sal / 35;
-
-//     let Ks = calc_Ks({Tc: Temp, Sal: Sal})
-
-//     let pH = 8.;  // start pH
-//     let H = 10 ** -pH;
-//     let diff_H = H;
-//     let tiny_diff_H = 1e-15;
-
-//     let iter = 0;
-//     let CA = null;
-//     let a = null;
-//     let b = null;
-//     let c = null;
-//     let H_old = null;
-
-//     while (diff_H > tiny_diff_H) {
-//         H_old = H;
-//         CA = TA - BT * (Ks.KB / (Ks.KB + H))
-        
-//         residual = TA - CA - BT * (Ks.KB / (Ks.KB + H))
-
-//         // a = CA;
-//         // b = Ks.K1 * (CA - DIC);
-//         // c = Ks.K1 * Ks.K2 * (CA - 2 * DIC);
-//         // H = (-b + Math.sqrt(b**2 - 4 * a * c)) / (2 * a);
-
-
-//         diff_H = Math.abs(H - H_old);
-//         iter += 1;
-//     }
-// }
-
-function calc_DIC({pCO2, TA, Temp, Sal}) {
-    TA = TA * 1e-6;
-    let CO2 = pCO2 * 1e-6 * Ks.K0;
-
-    let BT = 416e-6 * Sal / 35;
-
-    let Ks = calc_Ks({Tc: Temp, Sal: Sal})
-
-    let pHguess = 8.;
-    let pHtol = 0.0000001;
-    let deltapH = pHtol + 1;
-    let ln10 = Math.log(10)
-
-    while (deltapH > pHtol) {
-
-        // Insert routine from cbsyst - returns pH
-
-
-        deltapH = Math.abs(...)
-    }
-
-    // Calculate DIC from pH and CO2?
-    
-}
