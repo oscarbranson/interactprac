3f61abf6ef867c122e073220dd811246b10f19de
Oscar Branson <oscarbranson@gmail.com>
2020-09-17 14:08:15 +0100

    controls to sliders

--- carbon-live-js/src/components/ControlElements.jsx
+++ carbon-live-js/src/components/ControlElements.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Slider from 'react-rangeslider'
+import 'react-rangeslider/lib/index.css'
 
 export class Button extends React.Component {
     constructor(props) {
@@ -20,3 +22,36 @@ export class Button extends React.Component {
     }
 
 }
+
+export class VerticalSlider extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this)
+        this.fmtLabel = this.fmtLabel.bind(this)
+    }
+
+    handleChange = value => {
+        this.props.handleChange(value)
+      };
+
+    fmtLabel(value) {
+        return this.props.fmt_info[0](value, this.props.fmt_info[1])
+    }
+
+    render() {
+        return (
+            <Slider 
+                value={this.props.value}
+                min={this.props.min}
+                max={this.props.max}
+                orientation='vertical'
+                onChange={this.handleChange}
+                format={this.fmtLabel}
+                labels={this.props.labels}
+            />
+        )
+    }
+
+}
+
--- carbon-live-js/src/components/ModelControls.jsx
+++ carbon-live-js/src/components/ModelControls.jsx
@@ -1,21 +1,47 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+// import { Button } from 'react-bootstrap';
+import { VerticalSlider } from './ControlElements';
+import { start_state } from './ThreeBox_full'
 
-const paramFormats = {
-    vthermo: ['sci', 2],
-    vmix: ['sci', 2],
-    tau_hilat: ['', 1],
-    tau_lolat: ['', 1],
-    percent_CaCO3_hilat: ['', 1],
-    percent_CaCO3_lolat: ['', 1],
+function fmt_sci(value, precision) {
+    return value.toExponential(precision)
 }
 
-function formatnum(num, format) {
-    if (format[0] === 'sci') {
-        return num.toExponential(format[1])
-    } else {
-        return num.toPrecision(format[1])
-    }
+function fmt_pre(value, precision) {
+    return value.toPrecision(precision)
+}
+
+const paramFormats = {
+    vthermo: [fmt_sci, 2, start_state.vthermo * 0.5, start_state.vthermo * 2],
+    vmix: [fmt_sci, 2, start_state.vmix * 0.5, start_state.vmix * 2],
+    tau_hilat: [fmt_pre, 2, 1, 80],
+    tau_lolat: [fmt_pre, 2, 1, 80],
+    percent_CaCO3_hilat: [fmt_pre, 2, 0, 50],
+    percent_CaCO3_lolat: [fmt_pre, 2, 0, 50],
+    temp_hilat: [fmt_pre, 2, 0, 10],
+    temp_lolat: [fmt_pre, 2, 20, 30]
+}
+
+const sliderLabels = {
+    vthermo: {[start_state.vthermo]: '', [start_state.vthermo * 2]: 'x2', [start_state.vthermo / 2]: '/2'},
+    vmix: {[start_state.vmix]: '', [start_state.vmix * 2]: 'x2', [start_state.vmix / 2]: '/2'},
+    tau_hilat: {[start_state.tau_hilat]: ''},
+    tau_lolat: {[start_state.tau_lolat]: ''},
+    percent_CaCO3_hilat: {[start_state.percent_CaCO3_hilat]: ''},
+    percent_CaCO3_lolat: {[start_state.percent_CaCO3_lolat]: ''},
+    temp_hilat: {[start_state.temp_hilat]: ''},
+    temp_lolat: {[start_state.temp_lolat]: ''},
+}
+
+const labels = {
+    vthermo: 'V<sub>thermo</sub>',
+    vmix: 'V<sub>mix</sub>',
+    tau_hilat: '&tau;',
+    tau_lolat: '&tau;',
+    percent_CaCO3_hilat: '% CaCO<sub>3</sub>',
+    percent_CaCO3_lolat: '% CaCO<sub>3</sub>',
+    temp_hilat: 'Temp',
+    temp_lolat: 'Temp',
 }
 
 export class ControlSet extends React.Component {
@@ -23,6 +49,7 @@ export class ControlSet extends React.Component {
         super(props)
         this.handleDouble = this.handleDouble.bind(this);
         this.handleHalve = this.handleHalve.bind(this);    
+        this.handleSliderChange = this.handleSliderChange.bind(this);    
     }
 
     handleDouble(e) {
@@ -33,15 +60,33 @@ export class ControlSet extends React.Component {
         this.props.handleUpdate(this.props.param, this.props.value / 2)
     }
 
+    handleSliderChange(value) {
+        this.props.handleUpdate(this.props.param, value)
+    }
+
+    // render () {
+    //     return (
+    //         <div className="control-set">
+    //             <h3 dangerouslySetInnerHTML={{__html: labels[this.props.param]}}></h3>
+    //             <Button onClick={this.handleDouble} size='sm'>Double</Button>
+    //             <div className="control-value">
+    //                 {formatnum(this.props.value, paramFormats[this.props.param])}
+    //             </div>
+    //             <Button onClick={this.handleHalve} size='sm'>Halve</Button>
+    //             <VerticalSlider value={10} min={0} max={20}/>
+    //         </div>
+    //     )
+    // } 
     render () {
+        let min = paramFormats[this.props.param][2];
+        let max = paramFormats[this.props.param][3];
         return (
             <div className="control-set">
-                <h3>{this.props.param}</h3>
-                <Button onClick={this.handleDouble} size='sm'>Double</Button>
+                <h3 dangerouslySetInnerHTML={{__html: labels[this.props.param]}}></h3>
+                <VerticalSlider value={this.props.value} min={min} max={max} handleChange={this.handleSliderChange} fmt_info={paramFormats[this.props.param]} labels={sliderLabels[this.props.param]}/>
                 <div className="control-value">
-                    {formatnum(this.props.value, paramFormats[this.props.param])}
+                {paramFormats[this.props.param][0](this.props.value, paramFormats[this.props.param][1])}
                 </div>
-                <Button onClick={this.handleHalve} size='sm'>Halve</Button>
             </div>
         )
     } 
@@ -62,8 +107,11 @@ export class ModelControls extends React.Component {
         }
 
         return (
-            <div id="model-controls">
-                {controls}
+            <div className="control-section">
+                <h2>{this.props.title}</h2>
+                <div className="model-controls">
+                    {controls}
+                </div>
             </div>
         )
     } 
