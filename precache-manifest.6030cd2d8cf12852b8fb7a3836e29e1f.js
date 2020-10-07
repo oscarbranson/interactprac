5d86262ad0eea27e161246b2c3511dec4180b4a8
Oscar Branson <ob266@cam.ac.uk>
2020-10-08 00:24:34 +0100

    Updates

---
+++ precache-manifest.6030cd2d8cf12852b8fb7a3836e29e1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43eba92dc7ffdb4a4ea2c32c68fc2eac",
+    "url": "/interactprac/index.html"
+  },
+  {
+    "revision": "c7ca981726d93ba64452",
+    "url": "/interactprac/static/css/2.d9d52eda.chunk.css"
+  },
+  {
+    "revision": "ca57614c6c7dae32fca6",
+    "url": "/interactprac/static/css/main.ccba2dae.chunk.css"
+  },
+  {
+    "revision": "c7ca981726d93ba64452",
+    "url": "/interactprac/static/js/2.7da0fb3c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/interactprac/static/js/2.7da0fb3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca57614c6c7dae32fca6",
+    "url": "/interactprac/static/js/main.e350e06e.chunk.js"
+  },
+  {
+    "revision": "9b7f5051bf0b3c1d3f3e",
+    "url": "/interactprac/static/js/runtime-main.cc629ad0.js"
+  }
+]);
