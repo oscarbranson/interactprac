fce475e305f745919ff3da32fc9286db93fa900e
Oscar Branson <oscarbranson@gmail.com>
2020-10-19 13:37:00 +0100

    fix graph x axis

--- carbon-live-js/src/components/Graph.jsx
+++ carbon-live-js/src/components/Graph.jsx
@@ -90,7 +90,7 @@ export class Graph extends Component {
     this.graph.xAxis = d3.axisBottom(this.graph.xScale).ticks(2)
     this.graph.svg.append('g')
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + 65 + ")")
+      .attr("transform", "translate(0," + (100 - axis_bottom) + ")")
       .call(this.graph.xAxis)
 
     this.graph.lines = []
